0493fdb739ab5d074e5693e785b396e708a53b3d
skwon2345 <skwon2345@gmail.com>
2020-10-13 15:20:47 +0900

    third commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 

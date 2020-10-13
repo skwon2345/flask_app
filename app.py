2b1e5692e74800d9b213d4a5bb513fc6f0fc7444
skwon2345 <skwon2345@gmail.com>
2020-10-13 14:53:20 +0900

    post commit

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'This ifss the contenet of Post 1.',
-        'author': 'Aaron'
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is the contenet of Post 2.'
-    },
-]
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/posts')
-def posts():
-    return render_template('posts.html', posts=all_posts)
-
-@app.route('/home/<int:id>')
-def hello(id):
-    return "Hello, " + str(id)
-
-@app.route('/onlyget', methods=['GET'])
-def get_req():
-    return 'You can only get this webpage.'
+@app.route('/users', methods=['POST'])
+def createUser():
+  print(request.json)
+  id = db.insert({
+    'name': request.json['name'],
+    'email': request.json['email'],
+    'password': request.json['password']
+  })
+  return jsonify(str(ObjectId(id)))
 
 if __name__ == "__main__":
     app.run(debug=True)

aa6b57338dbe504a846eb186b57d5fa80c66dccf
skwon2345 <skwon2345@gmail.com>
2020-10-18 22:40:03 +0900

    saved'

--- main.py
+++ main.py
@@ -82,6 +82,3 @@ def getUsers():
 ##      'email': user['email'],
 ##      'password': user['password']
 ##  })
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
